48b56fb1b3f89096a95ebb5ee889bfd98019da54
Maecena <maya.demi.goddess@gmail.com>
2025-03-01 19:01:33 -0400

    Update mainFlashCard.py
    added meg's menu, added way to add questions

--- mainFlashCard.py
+++ mainFlashCard.py
@@ -1,8 +1,7 @@
 import random
 
-##def main():
-##    while True:
-##        addQuestion()
+def main():
+    runMainMenu()
 
 easy = -5
 hard = 5
@@ -10,12 +9,43 @@ hard = 5
 #holds the flash cards, key is a number and class Card is the card
 questionList = []  
 
+
+def runMainMenu():
+    while True:
+        options = {
+            1: ("Add Cards To Current List", "add card"),
+            2: ("Do A Set Of Questions", "play"),
+            3: ("Change Current List", "change list"),
+            4: ("Quit", "quit")
+        }
+        nextAction = displayMenuPrompt(options)
+
+        if nextAction == "add card":
+            #does it work?
+            addLotsOfQuestions()
+
+        elif nextAction == "play":
+            #does nothing
+            runSetOfQuestions()
+
+        elif nextAction == "change list":
+            #does nothing
+            changeList()
+            
+        elif nextAction == "quit":
+            #works!
+            print ("Goodbye!")
+            exit(0)
+
+
+
 class Card:
     def __init__(self, answer, question):
         self.level = "regular"
         self.challenge = 0
         self.answer = answer
         self.question = question
+        self.history = []
         
     #adds or subtracts from the challenge level
     def correct(self):
@@ -25,9 +55,11 @@ class Card:
         if choice == "y":
             self.challenge -= 1
             levelCheck(self)
+            self.history.append("y")
         elif choice == "n":
             self.challenge += 1
             levelCheck(self)
+            self.history.append("n")
 
     #checks to see what the level should be
     def levelCheck(self):
@@ -44,7 +76,63 @@ def addQuestion():
     answer = input("And the answer is?")
     questionList.append(Card(answer, question))
 
+def addLotsOfQuestions():
+    choice = None
+    while choice != "n":
+        choice = input("Add a question? y/n")
+        if choice != "n":
+            addQuestion()
 
-##FUTURE
+#
+def runSetOfQuestions():
+    pass
 
-#make lists based
+
+#
+def changeList():
+    pass
+    
+
+##FUTURE FEATURES
+
+###save and load stuff
+
+###ask questions from list, 5 random, X random
+
+###make lists based on self.level
+
+##view all questions
+
+###delete unwanted cards
+
+###make different sets of question lists (dictionary of lists)
+
+###delete old lists
+
+##scores and saving scores per set of questions
+
+###type in answer mode
+
+#Meg's code... makes the numbered menus work
+def displayMenuPrompt(options):
+    while True:
+        for num, item in options.items():
+            print (str(num) + " - " + item[0])
+
+        choice = input(">")
+
+        if not choice.isdigit():
+            print ("Please pick a number from 1 to %d. \n" % len(options))
+            continue
+
+        choice = int(choice)
+        if choice > 0 and choice <= len(options):
+            (optionDisplayText, optionInternalText) = options[choice]
+            print ("You picked option %d - %s\n" % (choice, optionDisplayText))
+            return optionInternalText
+        else:
+            print("Please pick a number from 1 to %d. \n" % len(options))
+
+
+
+main()
